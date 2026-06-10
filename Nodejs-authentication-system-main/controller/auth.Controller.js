69aebcb23584ffd06aa7658c57ea237985f860e1
BandariManasa19 <237r1a6669@cmrtc.ac.in>
2026-06-10 15:23:47 +0530

    Updated app.js and auth controller

--- Nodejs-authentication-system-main/controller/auth.Controller.js
+++ Nodejs-authentication-system-main/controller/auth.Controller.js
@@ -72,7 +72,7 @@ module.exports.login = async (req, res, next) => {
         );
 
         res.status(200).json({
-            message: "Login successful by manasa",
+            message: "Login successful by Bandarimanasa",
             token: token,
             user: exUser
         });
